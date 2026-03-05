164b9516eec774f7e302f61d623928f2c1a5785b
reclursive <imafraidofferrets@gmail.com>
2026-03-05 13:49:30 -0500

    Template folder up to app from config

--- config/settings.py
+++ config/settings.py
@@ -52,13 +52,20 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
-
 TEMPLATES = [
     {
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [BASE_DIR / 'templates'],
-    'APP_DIRS': True,
-}
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'], 
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request', 
+                'django.contrib.auth.context_processors.auth',  
+                'django.contrib.messages.context_processors.messages', 
+            ],
+        },
+    },
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
