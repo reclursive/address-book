a19e90f73074e0a1628bad845702343d5cc23ff8
reclursive <imafraidofferrets@gmail.com>
2026-03-07 13:39:46 -0500

    build.sh added and checks

--- manage.py
+++ manage.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
+
+from django.db import connection
+print(connection.settings_dict["ENGINE"])
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    # Ensure Django knows which settings to use
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'address_book_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
 
