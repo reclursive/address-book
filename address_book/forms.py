1738c8d006d47e4ab9661287e562304e0ea8340c
reclursive <imafraidofferrets@gmail.com>
2026-03-08 20:46:01 -0400

    added bday optional option, will include balloon or color change  near birthday

--- address_book/forms.py
+++ address_book/forms.py
@@ -2,8 +2,16 @@ from django import forms
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
-    email = forms.EmailField(required=False) 
+    email = forms.EmailField(required=False)
+
+    birthday = forms.DateField(
+        required=False,
+        widget=forms.DateInput(attrs={
+            'type': 'date',
+            'placeholder': 'Birthday'
+        })
+    )
 
     class Meta:
         model = Contact
-        fields = ['first_name', 'last_name', 'email', 'big_fan']
+        fields = ['first_name', 'last_name', 'email', 'big_fan', 'birthday']
